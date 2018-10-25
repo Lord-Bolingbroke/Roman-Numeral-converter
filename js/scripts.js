a579158ecb26e3583740f05d7f0ae8a11ce00a60
Ngan Nguyen and Noah Schooler <ngan@>
2018-10-25 13:51:35 -0700

    add working scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,103 @@
-romNum = [];
+function moveConsonants(word){
+  var wordArray = word.split('');
 
-function lessThanThree (number) {
-  if (number < 3) {
-    for (var i = 0; i < 3; i++) {
-      romNum.push("I")
+  var lastCharMoved;
+
+  for ( var i = 0 ; i < wordArray.length; i++){
+    if(isConsonant(wordArray[0]) || (!lastCharMoved && isY(wordArray[0]))) {
+      lastCharMoved = moveFirstCharToEnd(wordArray);
+    } else if((wordArray[0] == "u") && (lastCharMoved === "q")) {
+      moveFirstCharToEnd(wordArray);
     }
-  } return romNum.join("");
-}
-
-function lessThanEight (number) {
-  if (number > 4 && number <= 8) {
-    for (var i = 0; i < 3; i++) {
-      romNum.push("")
-    }
-  } return romNum.join("");
-}
-
-var countThree = function() {
-  for (var i = 0; i < 3; i++) {
-    console.log(i);
   }
-  return one(i);
+
+  return wordArray.join("") + "ay" ;
 }
 
-//
-// function group (number) {
-//   if (number < 3) {
-//     return "Use I";
-//   } else if (number > 3 || number < 10) {
-//     return "Use V"
-//   } else if (number > 10) {
-//     return "use X"
+
+
+// function LessThanThree(number){
+//   if (number <= 3) {
+//     return true;
 //   }
 //   return false;
 // }
 //
-// number = 11 = X
-// output = (number/5)
-// if number was 11 = vv +I = for more than 2 v = x
+// function divisibleByTen(number){
+//   if (number > 10) {
+//    return "X" + divisibleByFive(number % 10)
+//   }
+//   return false;
+// }
+//
+// function divisibleByFive(number){
+//   if (number > 5 && number < 10) {
+//    return "V" + addI(number % 5)
+//   }
+//   return false;
+// }
+//
+// // working function for less than 3
+// function addI(number){
+//   var firstArray = [];
+//    for( var i = 0 ; i < number; i++) {
+//     if (LessThanThree(number)) {
+//       firstArray.push("I");
+//       console.log("firstarray", firstArray);
+//     }
+//   }
+//   return  firstArray.join('');
+// }
+
+function convertNumber (number) {
+  if (number <= 0) {
+    return "There are no Roman Numeral equivalents for 0 or negative numbers"
+  } else if (addI(number)) {
+    return addI(number)
+  } else if (divisibleByFive(number)) {
+    return divisibleByFive(number)
+  } else if (divisibleByTen(number)) {
+    return divisibleByTen(number)
+  }
+}
+
+function LessThanThree(number){
+  if (number <= 3) {
+    return true;
+  }
+  return false;
+}
+
+function remainderOfTen (remainder) {
+  if (remainder < 5) {
+    addI(remainder)
+  } else if (remainder > 5) {
+    divisiblebyFive(remainder)
+  }
+}
+
+function divisibleByTen(number){
+  if (number >= 10) {
+   return "X" + divisibleByFive(number % 10)
+  }
+  return false;
+}
+
+function divisibleByFive(number){
+  if (number >= 5 && number < 10) {
+   return "V" + addI(number % 5)
+  }
+  return false;
+}
+
+// working function for less than 3
+function addI(number){
+  var firstArray = [];
+   for( var i = 0 ; i < number; i++) {
+    if (LessThanThree(number)) {
+      firstArray.push("I");
+      console.log("firstarray", firstArray);
+    }
+  }
+  return  firstArray.join('');
+}
