ae460ae6d9413eb01cde8f90d17743402861ab6b
Ngan Nguyen and Noah Schooler <ngan@>
2018-10-25 15:25:17 -0700

    clarify and remove superfluous code

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,39 @@
 function convertNumber (number) {
   if (number <= 0) {
     return "There are no Roman Numeral equivalents for 0 or negative numbers"
-  } else if (lessThanThree(number)) {
-    return lessThanThree(number)
-  } else if (divisibleByFive(number)) {
-    return divisibleByFive(number)
-  } else if (divisibleByTen(number)) {
-    return divisibleByTen(number)
-  }
-}
-
-function lessThanThree(number){
-  var firstArray = [];
-   for( var i = 0 ; i < number; i++) {
-    if (number <= 3) {
-      firstArray.push("I");
-      console.log("firstarray", firstArray);
-      console.log(number)
-    }
-  }
-  return firstArray.join('');
-}
-
-function divisibleByFive(number){
-  if (number === 4) {
-    return "IV";
+  } else if (number <= 3) {
+    return threeOrLess(number)
+  } else if (number === 4) {
+    return "IV"
+  } else if (number >= 5 && number < 9) {
+    return betweenFourAndNine(number)
   } else if (number === 9) {
     return "IX"
-  } else if (number >= 5 && number < 10) {
-   return "V" + lessThanThree(number % 5)
+  } else if (number >= 10) {
+    return greaterThanTen(number)
   }
-  return false;
 }
 
-function divisibleByTen(number){
-  if (number >= 10) {
-   return "X" + remainderOfTen(number % 10)
+function threeOrLess(number){
+  var romanNum = [];
+   for( var i = 0 ; i < number; i++) {
+      romanNum.push("I");
   }
-  return false;
+  return romanNum.join('');
+}
+
+function betweenFourAndNine(number){
+  return "V" + threeOrLess(number % 5)
+}
+
+function greaterThanTen(number){
+  return "X" + remainderOfTen(number % 10)
 }
 
 function remainderOfTen (remainder) {
   if (remainder < 5) {
-    lessThanThree(remainder);
-    console.log(remainder)
-  } else if (remainder > 4) {
-    divisibleByFive(remainder);
-    console.log(remainder);
+    return threeOrLess(remainder);
+  } else if (remainder >= 5) {
+    return betweenFourAndNine(remainder);
   }
 }
