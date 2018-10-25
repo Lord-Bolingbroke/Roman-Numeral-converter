c15735ee0e3de1d8d458f5070c48392ff2198004
Ngan Nguyen and Noah Schooler <noah@>
2018-10-25 14:47:41 -0700

    remove superfluous code

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,8 @@
-function moveConsonants(word){
-  var wordArray = word.split('');
-
-  var lastCharMoved;
-
-  for ( var i = 0 ; i < wordArray.length; i++){
-    if(isConsonant(wordArray[0]) || (!lastCharMoved && isY(wordArray[0]))) {
-      lastCharMoved = moveFirstCharToEnd(wordArray);
-    } else if((wordArray[0] == "u") && (lastCharMoved === "q")) {
-      moveFirstCharToEnd(wordArray);
-    }
-  }
-
-  return wordArray.join("") + "ay" ;
-}
-
-
-
-// function LessThanThree(number){
-//   if (number <= 3) {
-//     return true;
-//   }
-//   return false;
-// }
-//
-// function divisibleByTen(number){
-//   if (number > 10) {
-//    return "X" + divisibleByFive(number % 10)
-//   }
-//   return false;
-// }
-//
-// function divisibleByFive(number){
-//   if (number > 5 && number < 10) {
-//    return "V" + addI(number % 5)
-//   }
-//   return false;
-// }
-//
-// // working function for less than 3
-// function addI(number){
-//   var firstArray = [];
-//    for( var i = 0 ; i < number; i++) {
-//     if (LessThanThree(number)) {
-//       firstArray.push("I");
-//       console.log("firstarray", firstArray);
-//     }
-//   }
-//   return  firstArray.join('');
-// }
-
 function convertNumber (number) {
   if (number <= 0) {
     return "There are no Roman Numeral equivalents for 0 or negative numbers"
-  } else if (addI(number)) {
-    return addI(number)
+  } else if (lessThanThree(number)) {
+    return lessThanThree(number)
   } else if (divisibleByFive(number)) {
     return divisibleByFive(number)
   } else if (divisibleByTen(number)) {
@@ -61,43 +10,42 @@ function convertNumber (number) {
   }
 }
 
-function LessThanThree(number){
-  if (number <= 3) {
-    return true;
+function lessThanThree(number){
+  var firstArray = [];
+   for( var i = 0 ; i < number; i++) {
+    if (number <= 3) {
+      firstArray.push("I");
+      console.log("firstarray", firstArray);
+      console.log(number)
+    }
+  }
+  return firstArray.join('');
+}
+
+function divisibleByFive(number){
+  if (number === 4) {
+    return "IV";
+  } else if (number === 9) {
+    return "IX"
+  } else if (number >= 5 && number < 10) {
+   return "V" + lessThanThree(number % 5)
+  }
+  return false;
+}
+
+function divisibleByTen(number){
+  if (number >= 10) {
+   return "X" + remainderOfTen(number % 10)
   }
   return false;
 }
 
 function remainderOfTen (remainder) {
   if (remainder < 5) {
-    addI(remainder)
-  } else if (remainder > 5) {
-    divisiblebyFive(remainder)
+    lessThanThree(remainder);
+    console.log(remainder)
+  } else if (remainder > 4) {
+    divisibleByFive(remainder);
+    console.log(remainder);
   }
-}
-
-function divisibleByTen(number){
-  if (number >= 10) {
-   return "X" + divisibleByFive(number % 10)
-  }
-  return false;
-}
-
-function divisibleByFive(number){
-  if (number >= 5 && number < 10) {
-   return "V" + addI(number % 5)
-  }
-  return false;
-}
-
-// working function for less than 3
-function addI(number){
-  var firstArray = [];
-   for( var i = 0 ; i < number; i++) {
-    if (LessThanThree(number)) {
-      firstArray.push("I");
-      console.log("firstarray", firstArray);
-    }
-  }
-  return  firstArray.join('');
 }
